dd6c8600f6b480cd859d6b97443bef57c01b46a1
GrPaladino <paladino.graziano@gmail.com>
2024-01-17 15:22:46 +0100

    Feat: function palindroma

--- Palindromia/Js/main.js
+++ Palindromia/Js/main.js
@@ -6,8 +6,25 @@ while (!isNaN(userWord)) {
   //     - Mando un alert di errore e richiedo la parola
   alert("La parola inserita non é valida");
   userWord = prompt("Inserisci una parola");
-  console.log(userWord);
 }
 
 // - Creo una funzione per il controllo della parola
-//   - Controllo con un ciclo for che l parola é palindroma
+
+function checkWord() {
+  let reverseWord = "";
+  for (i = userWord.length - 1; i >= 0; i--) {
+    reverseWord += userWord[i];
+  }
+  return reverseWord;
+}
+
+let reverseWord = checkWord();
+c;
+while (userWord != reverseWord) {
+  alert("La parola inserita non é palindroma!! Inserire un`altra parola");
+  prompt("Inserisci una parola");
+}
+
+if (userWord === reverseWord) {
+  alert("Corretto");
+}
