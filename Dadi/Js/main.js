a2986863ae5c2eaef26827c0c76d915faf71fbfb
GrPaladino <paladino.graziano@gmail.com>
2024-01-17 17:12:31 +0100

    Feat: add dadi script

--- Dadi/Js/main.js
+++ Dadi/Js/main.js
@@ -1,1 +1,60 @@
-console.log("js ok");
+// - chiedo all'utente pari e dispari e un numero da 1 a 5 con un prompt
+const userType = prompt("Scegli pari o dispari");
+while (userType != "pari" && userType != "dispari") {
+  alert("Il valore inserito non é corretto");
+  userType = prompt("Scegli pari o dispari");
+}
+console.log(userType);
+
+let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+while (userNumber > 5) {
+  alert("Il valore inserito non é corretto");
+  userNumber = prompt("Inserisci un numero da 1 a 5");
+}
+console.log(userNumber);
+
+// - generiamo un numero da 1 a 5 (con una funzione)
+function getRandomNumber(min, max) {
+  min = parseInt(min);
+  max = parseInt(max);
+
+  if (isNaN(min) || isNaN(max)) {
+    console.error("I valori inseriti devono essere numerici");
+    return;
+  }
+  if (min >= max) {
+    console.error("Il valore massimo deve essere maggiore del valore minimo");
+    return;
+  }
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNumber;
+}
+
+const computerNumber = getRandomNumber(1, 5);
+console.log(computerNumber);
+
+// - facciamo la somma dei due numeri
+const addedNumber = userNumber + computerNumber;
+console.log(addedNumber);
+
+// - stabilisco se la somma dei due numeri é pari o dispari (con una funzione)
+
+function isEven(addedNumber) {
+  return addedNumber % 2 === 0;
+}
+
+let addedType = "";
+
+if (isEven(addedNumber)) {
+  addedType = "pari";
+} else {
+  addedType = "dispari";
+}
+
+// - se il numero corrisponde a cio' che ha scelto l'utente in precedenza (pari o dispari)
+// - l'utente ha vinto
+if (userType === addedType) {
+  alert("l`unte ha vinto");
+} else {
+  alert("l`utente ha perso");
+}
--- Dadi/README.md
+++ Dadi/README.md
@@ -1,4 +1,5 @@
-Pari e Dispari
+# Pari e Dispari
+
 L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 Sommiamo i due numeri
@@ -9,3 +10,12 @@ Consigli del giorno
 
 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+## SVOLGIMENTO
+
+- chiedo all'utente pari e dispari e un numero da 1 a 5 con un prompt
+- generiamo un numero da 1 a 5 (con una funzione)
+- facciamo la somma dei due numeri
+- stabilisco se la somma dei due numeri é pari o dispari (con una funzione)
+- se il numero corrisponde a cio' che ha scelto l'utente in precedenza (pari o dispari)
+- l'utente ha vinto
